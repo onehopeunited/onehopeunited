6fbd83e91a1499a2e6115d5367a16719c8b2d02c
TBozhok <toss@toss.od>
2013-12-05 11:24:55 +0200

    florida

--- wp-content/themes/ohu/page-careers-florida.php
+++ wp-content/themes/ohu/page-careers-florida.php
@@ -81,7 +81,7 @@ get_header(); ?>
                         FROM wp_posts, wp_postmeta
                         WHERE wp_posts.ID = wp_postmeta.post_id
                         AND wp_postmeta.meta_key = 'job-region'
-                        AND wp_postmeta.meta_value = 'Northern'
+                        AND wp_postmeta.meta_value = 'Florida'
                         AND wp_posts.post_status = 'publish'
                         AND wp_posts.post_type = 'job_listings')
                     ORDER BY wp_postmeta.meta_value ASC;", OBJECT);
