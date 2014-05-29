32731ed64510eebcaeecdbf1c76f5426bd1efba4
okononenko <okononenko@speroteck.com>
2014-05-29 18:50:29 +0300

    #34 Update calendar page twitter feed

--- wp-content/themes/ohu/page-blue-ribbon-calendar.php
+++ wp-content/themes/ohu/page-blue-ribbon-calendar.php
@@ -8,8 +8,8 @@ get_header(); ?>
 <div id="primary1">
     <div id="content1" role="main">
 
-        <div style="float:left;width:270px;padding-right:30px;border-right:1px solid #ccc;margin-top:50px;margin-bottom:30px;height:100%;">
-            <div style="border-top:1px solid #ccc;margin-right:-30px;margin-bottom:15px;">
+        <div style="float:left;width:300px;/*width:270px;padding-right:30px;*/border-right:1px solid #ccc;margin-top:50px;margin-bottom:30px;height:100%;">
+            <div style="border-top:1px solid #ccc;/*margin-right:-30px;*/margin-bottom:15px;">
                 <img src="http://onehopeunited.org/wp-content/themes/ohu/images/get-involved.gif">
                 <a href="/blue-ribbon-month/"><img class="back-btn" src="/wp-content/themes/ohu/images/goblue/back-button.png" alt="go back"></a>
             </div>
@@ -19,52 +19,58 @@ get_header(); ?>
             <img src="http://onehopeunited.org/wp-content/themes/ohu/images/red-arrow.png">  <a href="/blue-ribbon-month-spread-the-word/" target="" style="color:#000">Spread the Word</a><br />
             <img src="http://onehopeunited.org/wp-content/themes/ohu/images/red-arrow.png">  <a href="https://donate.onehopeunited.org/goblue" style="color:#000">Donate Now!</a>
 
-            <table class="" style="margin-top:5px;">
-                <tr height="20px">
-                    <td style="padding-left:13px;" colspan="4"><span style="position:absolute; margin-top:0;">Goal $200,000 dollars</span></td>
-                </tr>
+<!--            <table class="" style="margin-top:5px;">-->
+<!--                <tr height="20px">-->
+<!--                    <td style="padding-left:13px;" colspan="4"><span style="position:absolute; margin-top:0;">Goal $200,000 dollars</span></td>-->
+<!--                </tr>-->
+<!---->
+<!--                <tr>-->
+<!--                    <td style="width:50px;height:80px;"><a href="https://donate.onehopeunited.org/blue-ribbon-month-2013"><img src="http://onehopeunited.org/wp-content/themes/ohu/images/yellow-icon.png"></a></td><td><span style="color:#676767;">DOLLARS <br /><b>Raised</b></span></td><td><center>=</center></td><td><span style="color:#e6a813">--><?php //query_posts( 'page_id=3526' );?>
+<!--                            --><?php //while ( have_posts() ) : the_post(); ?>
+<!--                                --><?php //echo get_post_meta($post->ID, 'dollars_raised', true) ?>
+<!--                            --><?php //endwhile; ?>
+<!--                            --><?php //wp_reset_query(); ?><!--</span>-->
+<!--                    </td>-->
+<!--                </tr>-->
+<!---->
+<!--                <tr height="20px">-->
+<!--                    <td style="padding-left:30px;" colspan="4"><span style="position:absolute; margin-top:0;">Goal 65 events</span></td>-->
+<!--                </tr>-->
+<!---->
+<!--                <tr>-->
+<!--                    <td style="width:50px;height:80px;"><a href="http://onehopeunited.org/blue-ribbon-month-host-an-event/"><img src="http://onehopeunited.org/wp-content/themes/ohu/images/red-icon.png"></a></td><td width=70><span style="color:#676767;">EVENTS <br /><b>Hosted</b></span></td><td width=20><center>=</center></td><td><span style="color:#d03b3e">--><?php //query_posts( 'page_id=3526' );?>
+<!--                        --><?php //while ( have_posts() ) : the_post(); ?>
+<!--                            --><?php //echo get_post_meta($post->ID, 'events_hosted', true) ?>
+<!--                        --><?php //endwhile; ?>
+<!--                        --><?php //wp_reset_query(); ?><!--</span>-->
+<!--                    </td>-->
+<!--                </tr>-->
+<!---->
+<!--                <tr height="20px">-->
+<!--                    <td style="padding-left:29px;" colspan="4"><span style="position:absolute; margin-top:0;">Goal 750 donors</span></td>-->
+<!--                </tr>-->
+<!---->
+<!--                <tr>-->
+<!--                    <td style="width:56px;height:80px;"><a href="http://onehopeunited.org/blue-ribbon-month-events/"><img src="http://onehopeunited.org/wp-content/themes/ohu/images/blue-icons.png"></a></td><td><span style="color:#676767;">DONORS <br /><b>Engaged</b></span></td><td><center>=</center></td><td><span style="color:#3993c4">-->
+<!--                        --><?php //query_posts( 'page_id=3526' );?>
+<!--                        --><?php //while ( have_posts() ) : the_post(); ?>
+<!--                        --><?php //echo get_post_meta($post->ID, 'donor_count', true) ?>
+<!--                        --><?php //endwhile; ?>
+<!--                        --><?php //wp_reset_query(); ?><!--</span>-->
+<!--                    </td>-->
+<!--                </tr>-->
+<!---->
+<!--            </table>-->
+<!---->
+<!--            <div class="download-kit">-->
+<!--                <span><a href="mailto:Goblue@onehopeunited.org">Have Questions? Contact Us </a></span>-->
+<!--                <a href="/wp-content/uploads/2014/03/OHU_GoBlue_CampaignKit_2014.pdf"><img src="/wp-content/themes/ohu/images/goblue/download-kit-01.jpg" alt="download GoBlue Campaign Kit"/></a>-->
+<!--            </div>-->
 
-                <tr>
-                    <td style="width:50px;height:80px;"><a href="https://donate.onehopeunited.org/blue-ribbon-month-2013"><img src="http://onehopeunited.org/wp-content/themes/ohu/images/yellow-icon.png"></a></td><td><span style="color:#676767;">DOLLARS <br /><b>Raised</b></span></td><td><center>=</center></td><td><span style="color:#e6a813"><?php query_posts( 'page_id=3526' );?>
-                            <?php while ( have_posts() ) : the_post(); ?>
-                                <?php echo get_post_meta($post->ID, 'dollars_raised', true) ?>
-                            <?php endwhile; ?>
-                            <?php wp_reset_query(); ?></span>
-                    </td>
-                </tr>
-
-                <tr height="20px">
-                    <td style="padding-left:30px;" colspan="4"><span style="position:absolute; margin-top:0;">Goal 65 events</span></td>
-                </tr>
-
-                <tr>
-                    <td style="width:50px;height:80px;"><a href="http://onehopeunited.org/blue-ribbon-month-host-an-event/"><img src="http://onehopeunited.org/wp-content/themes/ohu/images/red-icon.png"></a></td><td width=70><span style="color:#676767;">EVENTS <br /><b>Hosted</b></span></td><td width=20><center>=</center></td><td><span style="color:#d03b3e"><?php query_posts( 'page_id=3526' );?>
-                        <?php while ( have_posts() ) : the_post(); ?>
-                            <?php echo get_post_meta($post->ID, 'events_hosted', true) ?>
-                        <?php endwhile; ?>
-                        <?php wp_reset_query(); ?></span>
-                    </td>
-                </tr>
-
-                <tr height="20px">
-                    <td style="padding-left:29px;" colspan="4"><span style="position:absolute; margin-top:0;">Goal 750 donors</span></td>
-                </tr>
-
-                <tr>
-                    <td style="width:56px;height:80px;"><a href="http://onehopeunited.org/blue-ribbon-month-events/"><img src="http://onehopeunited.org/wp-content/themes/ohu/images/blue-icons.png"></a></td><td><span style="color:#676767;">DONORS <br /><b>Engaged</b></span></td><td><center>=</center></td><td><span style="color:#3993c4">
-                        <?php query_posts( 'page_id=3526' );?>
-                        <?php while ( have_posts() ) : the_post(); ?>
-                        <?php echo get_post_meta($post->ID, 'donor_count', true) ?>
-                        <?php endwhile; ?>
-                        <?php wp_reset_query(); ?></span>
-                    </td>
-                </tr>
-
-            </table>
-
-            <div class="download-kit">
-                <span><a href="mailto:Goblue@onehopeunited.org">Have Questions? Contact Us </a></span>
-                <a href="/wp-content/uploads/2014/03/OHU_GoBlue_CampaignKit_2014.pdf"><img src="/wp-content/themes/ohu/images/goblue/download-kit-01.jpg" alt="download GoBlue Campaign Kit"/></a>
+            <div>
+                <br />
+                <a class="twitter-timeline" width="300" height="450"  href="https://twitter.com/1hopeunited"  data-widget-id="472036111640821760">Tweets by @1hopeunited</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
             </div>
 
             <script src="http://widgets.twimg.com/j/2/widget.js"></script>
